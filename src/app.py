840b3abf960f64e42abbe42e98df5e193d665579
abkebir <90589457+abkebir@users.noreply.github.com>
2024-08-29 12:29:52 +0200

    Update flags in core app

--- src/app.py
+++ src/app.py
@@ -162,10 +162,10 @@ def display_footer():
     
     col1, col2 = st.columns([1, 1])  # Equal width for both columns
     with col1:
-        st.image("C:\\Users\\PMJ\\Documents\\projets_ETX\\RAG\\ALTAMURA\\flags\\EU_POS.jpeg", caption="European Union", width=170)
+        st.image("./flags/EU_POS.jpeg", caption="European Union", width=170)
         
     with col2:
-        st.image("C:\\Users\\PMJ\\Documents\\projets_ETX\\RAG\\ALTAMURA\\flags\\LOGO_OFAJ_CARTOUCHE_RVB_BLEU.jpeg", caption="Franco-German Youth Office", width=120)
+        st.image("./flags/LOGO_OFAJ_CARTOUCHE_RVB_BLEU.jpeg", caption="Franco-German Youth Office", width=120)
 
     
     # Contact Information
